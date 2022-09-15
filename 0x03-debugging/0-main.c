4610b1674f1dac2526d5226ed0467595f206f460
Nebi3233 <kanyinebicharles@gmail.com>
2022-09-15 10:10:17 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function that print if interger is positve or negetive
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+positive or negetive
+
+return (0);
+}
