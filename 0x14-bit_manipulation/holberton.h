2e2f04b2ac05d2cadeda034c950161f0794602a5
Nebi3233 <kanyinebicharles@gmail.com>
2022-10-27 14:52:06 -0700

    all project

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,13 +1,19 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-#include <stdlib.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+/*
+ * File: holberton.h
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-void _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-#endif /* _HOLBERTON_H_ */
+
+#endif /* HOLBERTON_H */
